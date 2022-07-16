e658dffb4d9bbb95299981de23d63ecfb976158c
HPHuerta <hhernanp@gmail.com>
2022-07-16 15:35:03 -0400

    add GET request to get a single inventory item, added route in server

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const inventoryRoute = require('./routes/inventory');
+
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-app.use(cors())
-app.use(express.json())
 
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send('success')
-})
-
-
-
+// inventory route
+app.use('/inventory', inventoryRoute);
 
 app.listen(PORT, () => {
-    console.log('connected to port 8080')
-})
+  console.log('connected to port 8080');
+});
