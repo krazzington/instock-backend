a9ab33afcb770dcb59f177b7b0b7d28baafd009d
sjpark538 <phi5388@gmail.com>
2022-07-16 15:44:17 -0400

    delete a warehouse

---
+++ routes/warehouse.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const uuid = require("uuid");
+const fs = require("fs");
+router.use(cors());
+
+const warehouses = require("../data/warehouses.json");
+
+router.delete ("/:id", (req,res) =>{
+    const deletedItem = warehouses.findIndex((item) => item.id === req.params.id);
+    warehouses.splice(deletedItem, 1);
+    fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouses));
+    res.send(warehouses)
+})
