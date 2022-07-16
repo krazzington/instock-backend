8637305076987d780b7260b17ea23eaa5c88dd7f
sjpark538 <phi5388@gmail.com>
2022-07-16 16:38:22 -0400

    push

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -7,9 +7,13 @@ router.use(cors());
 
 const warehouses = require("../data/warehouses.json");
 
+console.log(warehouses);
+
 router.delete ("/:id", (req,res) =>{
     const deletedItem = warehouses.findIndex((item) => item.id === req.params.id);
     warehouses.splice(deletedItem, 1);
     fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouses));
     res.send(warehouses)
 })
+
+module.exports = router;
