e0ab81d650b7ea2a4e72289dc45707d2d8fc22ba
krazzington <mkrazbookings@gmail.com>
2022-07-16 10:18:28 -0400

    added port, cors, express.json and basic api(?)

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+const PORT = 8080;
+
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('success')
+})
