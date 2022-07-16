5e11abfe57093d0c75b756e768581c9e7f8acc24
Javeland74 <Haveland74@gmail.com>
2022-07-16 15:15:48 -0600

    merging

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const router = express.Router();
 
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 let inventories = require('./data/inventories.json');
 let warehouses = require('./data/warehouses.json');
 
 app.get('/', (req, res) => {
-    res.status(200).send('success')
-})
+    res.status(200).send('success');
+});
+
+router.get('/warehouse/${id}', (req, res) => {
+    const sngWrhseID = req.params.id;
+    const sngWrhseInvList = warehouseData.find(warehouse => warehouse.id === sngWrhseID);
+    if (sngWrhseInvList) {
+        res.send(sngWrhseInvList);
+    }
+    else {
+        res.send("Warehouse Not Available");
+    }
+});
 
 
 
 
 app.listen(PORT, () => {
-    console.log('connected to port 8080')
-})
+    console.log('connected to port 8080');
+});
