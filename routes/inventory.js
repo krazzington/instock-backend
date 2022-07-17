a30c76b942adbd9e164708c710980436ae97774b
HPHuerta <hhernanp@gmail.com>
2022-07-16 20:01:03 -0400

    add DELETE route functionality to delete an item in inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,6 +1,7 @@
 // Require Express Router
 const router = require('express').Router();
 const inventory = require('../data/inventories.json');
+const fs = require('fs');
 
 //GET request to get an item
 router.get('/:id', (req, res) => {
@@ -21,6 +22,25 @@ router.get('/:id', (req, res) => {
 
     // response with item detail based on the id
     res.status(200).json(inventory.find((item) => item.id === req.params.id));
+  } catch (error) {
+    res.status(500);
+  }
+});
+
+// DELETE an inventory
+
+router.delete(`/:id`, (req, res) => {
+  try {
+    // Finding the index of the item in the inventory array.
+    const item = inventory.findIndex((item) => item.id === req.params.id);
+
+    // Removing the item from the inventory array.
+    inventory.splice(item, 1);
+
+    // Writing the inventory array to the inventory.json file.
+    fs.writeFileSync('./data/inventories.json', JSON.stringify(inventory));
+
+    res.status(200).send('Item Deleted');
   } catch (error) {
     res.status(500);
   }
