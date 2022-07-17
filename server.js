ff9bc5c7d9dc95a615415bb45d4dd39c9e6c7adb
HPHuerta <hhernanp@gmail.com>
2022-07-16 21:11:28 -0400

    add GET request endpoint to get all warehouses list. Cleaned up server.js

--- server.js
+++ server.js
@@ -6,24 +6,16 @@ const inventoryRoute = require('./routes/inventory');
 const warehouseRoute = require('./routes/warehouse');
 
 const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
-
-app.use(cors())
-app.use(express.json())
-
-let inventories = require('./data/inventories.json');
-let warehouses = require('./data/warehouses.json');
-
-app.get('/', (req, res) => {
-    res.status(200).send('success')
-})
-
 
 app.use(cors());
 app.use(express.json());
 
+let inventories = require('./data/inventories.json');
+let warehouses = require('./data/warehouses.json');
+
 // inventory route
 app.use('/inventory', inventoryRoute);
+// warehouse route
 app.use('/warehouse', warehouseRoute);
 
 app.listen(PORT, () => {
