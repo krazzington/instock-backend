217a8bb2fa9cdb17986a6a4932a0519819c1ea2c
krazzington <mkrazbookings@gmail.com>
2022-07-19 20:18:26 -0400

    post new warehouse works and validation works on the backend

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const warehouseRoute = require('./routes/warehouse')
 
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 app.use(cors())
 app.use(express.json())
 
+let inventories = require('./data/inventories.json');
+let warehouses = require('./data/warehouses.json');
 
 app.get('/', (req, res) => {
     res.status(200).send('success')
 })
 
-
+app.use('/warehouse', warehouseRoute)
 
 
 app.listen(PORT, () => {
